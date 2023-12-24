43692cc8cdf54b1b440907481e6cd31a645cdf04
kututa <vinnykututa@gmail.com>
2023-12-24 06:52:38 +0300

    init new smart contract

---
+++ README.md
@@ -0,0 +1,129 @@
+# School Placement System
+
+## Overview
+
+This smart contract implements a parking reservation system using the typescript on azle for internet Computer. The system allows for the initialization of an owner, addition of parking slots, allocation of parking spaces to clients, valet delivery, and various operations on parking slots.
+
+## Prerequisites
+
+- Node
+- Typescript
+- DFX
+- IC CDK
+
+## Installation
+
+1. **Clone the repository:**
+
+    ```bash
+    git clone https://github.com/Collins-Ruto/school-placement.git
+    cd school-placement
+    ```
+
+## Project Structure
+
+The project is organized into the following directories and files:
+
+- **`src/`**: Contains the source code for the school Placement System.
+  - **`index.ts`**: App entry point Implementation of the school Placement System.
+
+- **`node_modules/`**: Directory for project dependencies.
+
+- **`package.json`**: Configuration file for npm, including project dependencies and scripts.
+
+- **`tsconfig.json`**: TypeScript configuration file, specifying compiler options.
+
+- **`LICENSE`**: MIT License file, detailing the terms under which the project is licensed.
+
+- **`README.md`**: Project documentation providing an overview, installation instructions, usage details, and license information.
+
+## Functions
+
+### `initOwner(name: string): string`
+
+- Initializes the system owner with a unique ID, name, and timestamp.
+- There can only be one owner per contract.
+
+### `getOwner(): Owner`
+
+- Retrieves the details of the system owner.
+
+### `getAvailableSlots(): Result<Vec<Parking>, string>`
+
+- Retrieves a list of available parking slots.
+
+### `addParkingSlot(payload: ParkingPayload): string`
+
+- Adds a new parking slot to the system. Reserved for the contract owner.
+
+### `getParkingSpace(payload: AllocationPayload): string`
+
+- Allocates a parking space to a client, marking the slot as occupied.
+
+### `valetDelivery(payload: ValletPayload): string`
+
+- Handles vallet delivery, updating the total cost and client's location.
+
+### `pickupCar(id: string): { msg: string; price: number }`
+
+- Picks up a car from a parking slot, calculates the parking duration and cost.
+
+### `updateParkingSlot(id: string, payload: ParkingPayload): string`
+
+- Updates information for a parking slot, such as parking ID and price. Reserved for the contract owner.
+
+### `deleteParkingSlot(id: string): string`
+
+- Deletes a parking slot from the system. Reserved for the contract owner.
+
+## Usage
+
+- Initialize the owner using `initOwner(name)`.
+- Add parking slots with `addParkingSlot(payload)`.
+- Allocate parking spaces to clients using `getParkingSpace(payload)`.
+- Manage vallet delivery with `valetDelivery(payload)`.
+- Perform various operations on parking slots, including updates and deletions.
+
+## Try it out
+
+`dfx` is the tool you will use to interact with the IC locally and on mainnet. If you don't already have it installed:
+
+```bash
+npm run dfx_install
+```
+
+Next you will want to start a replica, which is a local instance of the IC that you can deploy your canisters to:
+
+```bash
+npm run replica_start
+```
+
+If you ever want to stop the replica:
+
+```bash
+npm run replica_stop
+```
+
+Now you can deploy your canister locally:
+
+```bash
+npm install
+npm run canister_deploy_local
+```
+
+To call the methods on your canister:
+
+```bash
+npm run name_of_function
+npm run name_of_function
+```
+
+Assuming you have [created a cycles wallet](https://internetcomputer.org/docs/current/developer-docs/quickstart/network-quickstart) and funded it with cycles, you can deploy to mainnet like this:
+
+```bash
+npm run canister_deploy_mainnet
+```
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ src/index.ts
@@ -0,0 +1,403 @@
+// Importing necessary modules from the 'azle' library and 'uuid' library
+import {
+  $query,
+  $update,
+  Record,
+  StableBTreeMap,
+  Vec,
+  match,
+  Result,
+  nat64,
+  ic,
+  Opt,
+  Principal,
+} from "azle";
+import { v4 as uuidv4 } from "uuid";
+
+// Defining record types for different entities
+type Ministry = Record<{
+  id: string;
+  ministry: Principal;
+}>;
+
+type Highschool = Record<{
+  id: string;
+  name: string;
+  phone: string;
+  level: SchoolLevel;
+  county: string;
+}>;
+
+type Placement = Record<{
+  id: string;
+  highschool: Principal;
+  student: Principal;
+}>;
+
+type Student = Record<{
+  id: string;
+  name: string;
+  phone: string;
+  grade: Grade;
+  county: string;
+  highschool: Highschool | undefined;
+}>;
+
+type HighschoolPayload = Record<{
+  name: string;
+  phone: string;
+  level: SchoolLevel;
+  county: string;
+}>;
+
+type UpdateHighschoolPayload = Record<{
+  id: string;
+  level: SchoolLevel;
+  phone: string;
+}>;
+
+enum SchoolLevel {
+  NATIONAL = "NATIONAL",
+  COUNTY = "COUNTY",
+  SUB_COUNTY = "SUB_COUNTY",
+  DISTRICT = "DISTRICT",
+  DAYSCHOOL = "DAYSCHOOL",
+}
+
+enum Grade {
+  A1 = "A+",
+  A2 = "A",
+  A3 = "A-",
+  B1 = "B+",
+  B2 = "B",
+  B3 = "B-",
+  C1 = "C+",
+  C2 = "C",
+  C3 = "C-",
+  D1 = "D+",
+  D2 = "D",
+  D3 = "D-",
+}
+
+type StudentPayload = Record<{
+  name: string;
+  phone: string;
+  grade: Grade;
+  county: string;
+}>;
+
+type CarResponse = Record<{
+  msg: string;
+  price: number;
+}>;
+
+// Creating instances of StableBTreeMap for each entity type
+const ministryStorage = new StableBTreeMap<string, Ministry>(0, 44, 512);
+const placementStorage = new StableBTreeMap<string, Placement>(1, 44, 512);
+const studentStorage = new StableBTreeMap<string, Student>(2, 44, 512);
+const highschoolStorage = new StableBTreeMap<string, Highschool>(3, 44, 512);
+
+// Initialization of ministryStorage
+$update;
+export function initMinistry(): Result<Ministry, string> {
+  try {
+    // Validate that the ministry has not been initialized already
+    if (!ministryStorage.isEmpty()) {
+      return Result.Err<Ministry, string>(
+        "Ministry has already been initialized"
+      );
+    }
+
+    // Create a new ministry
+    const ministry: Ministry = {
+      id: uuidv4(),
+      ministry: ic.caller(),
+    };
+
+    // Insert the ministry into ministryStorage
+    ministryStorage.insert(ministry.id, ministry);
+
+    return Result.Ok(ministry);
+  } catch (error) {
+    return Result.Err<Ministry, string>("Failed to initialize ministry");
+  }
+}
+
+// check if the person making the request is the ministry
+function isMinistry(caller: string): boolean {
+  const ministry = ministryStorage.values()[0];
+  return ministry.ministry.toText() !== caller;
+}
+
+$update;
+// Function to add a new highschool
+export function addHighschool(
+  payload: HighschoolPayload
+): Result<string, string> {
+  try {
+    // Validate that the ministry exists
+    if (ministryStorage.isEmpty()) {
+      return Result.Err("Ministry has not been initialized");
+    }
+
+    // Check if the caller is the ministry
+    if (isMinistry(ic.caller().toText())) {
+      return Result.Err("Action reserved for the ministry");
+    }
+
+    // Validate the payload
+    if (!payload.name || !payload.county || !payload.level || !payload.phone) {
+      return Result.Err("Incomplete input data!");
+    }
+
+    // Create a new highschool slot
+    const highschool: Highschool = {
+      id: uuidv4(),
+      name: payload.name,
+      phone: payload.phone,
+      level: payload.level,
+      county: payload.county,
+    };
+
+    // Insert the highschool slot into highschoolStorage
+    highschoolStorage.insert(highschool.id, highschool);
+
+    return Result.Ok(highschool.id);
+  } catch (error) {
+    return Result.Err("Failed to add highschool slot");
+  }
+}
+
+$query;
+// get highschool by id
+export function getHighschool(id: string): Result<Highschool, string> {
+  try {
+    // Validate the ID
+    if (!id) {
+      return Result.Err("Invalid ID");
+    }
+
+    // Get the highschool slot
+    const highschoolResult = highschoolStorage.get(id);
+
+    // Use match to handle the Result type
+    return match(highschoolResult, {
+      Some: (highschool) => Result.Ok<Highschool, string>(highschool),
+      None: () => Result.Err<Highschool, string>("Highschool slot not found"),
+    });
+  } catch (error) {
+    return Result.Err("Failed to get highschool slot");
+  }
+}
+
+$query;
+// get all highschools
+export function getAllHighschools(): Result<Vec<Highschool>, string> {
+  try {
+    // Get all highschools
+    const highschools = highschoolStorage.values();
+
+    // check if there are any highschools
+    return match(highschools, {
+      0: () => Result.Err<Vec<Highschool>, string>("No highschools found"),
+      _: () => Result.Ok<Vec<Highschool>, string>(highschools),
+    });
+  } catch (error) {
+    return Result.Err("Failed to get highschools");
+  }
+}
+
+$query;
+// search highshool by name
+export function searchHighschoolByName(
+  name: string
+): Result<Vec<Highschool>, string> {
+  try {
+    // Validate the name
+    if (!name) {
+      return Result.Err("Invalid name");
+    }
+
+    // Get all highschools
+    const highschools = highschoolStorage.values();
+
+    // Filter the highschools by name
+    const filteredHighschools = highschools.filter((highschool) =>
+      highschool.name.toLowerCase().includes(name.toLowerCase())
+    );
+
+    // check if there are any highschools
+    return match(filteredHighschools, {
+      0: () => Result.Err<Vec<Highschool>, string>("No highschools found"),
+      _: () => Result.Ok<Vec<Highschool>, string>(filteredHighschools),
+    });
+  } catch (error) {
+    return Result.Err("Failed to search highschools");
+  }
+}
+
+$query;
+// search highschool by county
+export function searchHighschoolByCounty(
+  county: string
+): Result<Vec<Highschool>, string> {
+  try {
+    // Validate the county
+    if (!county) {
+      return Result.Err("Invalid county");
+    }
+
+    // Get all highschools
+    const highschools = highschoolStorage.values();
+
+    // Filter the highschools by county
+    const filteredHighschools = highschools.filter((highschool) =>
+      highschool.county.toLowerCase().includes(county.toLowerCase())
+    );
+
+    // check if there are any highschools
+    return match(filteredHighschools, {
+      0: () => Result.Err<Vec<Highschool>, string>("No highschools found"),
+      _: () => Result.Ok<Vec<Highschool>, string>(filteredHighschools),
+    });
+  } catch (error) {
+    return Result.Err("Failed to search highschools");
+  }
+}
+
+$query;
+// search highschool by level
+export function searchHighschoolByLevel(
+  level: SchoolLevel
+): Result<Vec<Highschool>, string> {
+  try {
+    // Validate the level
+    if (!level) {
+      return Result.Err("Invalid level");
+    }
+
+    // Get all highschools
+    const highschools = highschoolStorage.values();
+
+    // Filter the highschools by level
+    const filteredHighschools = highschools.filter(
+      (highschool) => highschool.level === level
+    );
+
+    // check if there are any highschools
+    return match(filteredHighschools, {
+      0: () => Result.Err<Vec<Highschool>, string>("No highschools found"),
+      _: () => Result.Ok<Vec<Highschool>, string>(filteredHighschools),
+    });
+  } catch (error) {
+    return Result.Err("Failed to search highschools");
+  }
+}
+
+$update;
+//  function to add a new student
+export function addStudent(payload: StudentPayload): Result<Student, string> {
+  try {
+    // Validate the payload
+    if (!payload.name || !payload.county || !payload.grade || !payload.phone) {
+      return Result.Err("Incomplete input data!");
+    }
+
+    // Create a new student
+    const student: Student = {
+      id: uuidv4(),
+      name: payload.name,
+      phone: payload.phone,
+      grade: payload.grade,
+      county: payload.county,
+      highschool: undefined,
+    };
+
+    // Insert the student into studentStorage
+    studentStorage.insert(student.id, student);
+
+    return Result.Ok(student);
+  } catch (error) {
+    return Result.Err("Failed to add student");
+  }
+}
+
+$update;
+// Function to update information for a highschool slot
+export function updateHighschoolSlot(
+  payload: UpdateHighschoolPayload
+): Result<Highschool, string> {
+  try {
+    // Validate the ID
+    if (!payload.id) {
+      return Result.Err("Invalid ID");
+    }
+
+    // Check if the caller is the contract ministry
+    if (isMinistry(ic.caller().toText())) {
+      return Result.Err("Action reserved for the contract ministry");
+    }
+
+    // Validate the payload
+    if (!payload.level || !payload.phone) {
+      return Result.Err("Incomplete input data!");
+    }
+
+    // Get the highschool slot
+    const highschoolResult = highschoolStorage.get(payload.id);
+
+    // Use match to handle the Result type
+    return match(highschoolResult, {
+      Some: (highschool) => {
+        // Update the highschool slot information
+        highschool.level = payload.level;
+        highschool.phone = payload.phone;
+
+        // Insert the updated highschool slot into highschoolStorage
+        highschoolStorage.insert(highschool.id, highschool);
+
+        return Result.Ok<Highschool, string>(highschool);
+      },
+      None: () => Result.Err<Highschool, string>("Highschool slot not found"),
+    });
+  } catch (error) {
+    return Result.Err<Highschool, string>("Failed to update highschool slot");
+  }
+}
+
+$update;
+// Function to delete a highschool slot
+export function deleteHighschoolSlot(id: string): Result<string, string> {
+  try {
+    // Validate the ID
+    if (!id) {
+      return Result.Err("Invalid ID");
+    }
+
+    // Check if the caller is the contract ministry
+    if (isMinistry(ic.caller().toText())) {
+      return Result.Err("Action reserved for the contract ministry");
+    }
+
+    // Remove the highschool slot from highschoolStorage
+    highschoolStorage.remove(id);
+
+    return Result.Ok(`Highschool slot of ID: ${id} removed successfully`);
+  } catch (error) {
+    return Result.Err("Failed to delete highschool slot");
+  }
+}
+
+// Mocking the 'crypto' object for testing purposes
+globalThis.crypto = {
+  // @ts-ignore
+  getRandomValues: () => {
+    let array = new Uint8Array(32);
+
+    for (let i = 0; i < array.length; i++) {
+      array[i] = Math.floor(Math.random() * 256);
+    }
+
+    return array;
+  },
+};
